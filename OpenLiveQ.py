acf6853f7435e16d41499da003e1aaabd09dff94
Tomohiro Manabe <tmanabe@users.noreply.github.com>
2017-03-05 22:51:42 +0900

    add ClickThrough and QuestionData classes

--- OpenLiveQ.py
+++ OpenLiveQ.py
@@ -2,6 +2,49 @@
 # coding: utf-8
 
 
+class ClickThrough(dict):
+    keys = [
+        'query_id',
+        'question_id',
+        'mode_rank',
+        'ctr',
+        'male',
+        'female',
+        '10-',
+        '10s',
+        '20s',
+        '30s',
+        '40s',
+        '50s',
+        '60+',
+    ]
+    separator = '\t'
+
+    def __missing__(self, query_id):
+        self[query_id] = {}
+        return self[query_id]
+
+    def read(self, path):
+        count = 0
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                count += 1
+                if count % 20000 == 0:
+                    print(count)
+                d = {}
+                l = line.rstrip().split(ClickThrough.separator)
+                try:
+                    for i, key in enumerate(ClickThrough.keys):
+                        d[key] = l[i]
+                except IndexError:
+                    print('IndexError at %i' % count)
+                    continue
+                # print(d)
+                self[d['query_id']][d['question_id']] = d
+        assert count == 440163
+        return self
+
+
 class Query(dict):
     linebreak = '\n'
     separator = '\t'
@@ -19,6 +62,48 @@ class Query(dict):
             for k in sorted(list(self.keys())):
                 file.write(Query.separator.join([k, self[k]]))
                 file.write(Query.linebreak)
+        return self
+
+
+class QuestionData(dict):
+    keys = [
+        'query_id',
+        'rank',
+        'question_id',
+        'title',
+        'snippets',
+        'status',
+        'timestamp',
+        'answer_count',
+        'view_count',
+        'category',
+        'question_body',
+        'best_answer_body',
+    ]
+    separator = '\t'
+
+    def __missing__(self, query_id):
+        self[query_id] = []
+        return self[query_id]
+
+    def read(self, path):
+        count = 0
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                count += 1
+                if count % 20000 == 0:
+                    print(count)
+                d = {}
+                l = line.rstrip().split(QuestionData.separator)
+                try:
+                    for i, key in enumerate(QuestionData.keys):
+                        d[key] = l[i]
+                except IndexError:
+                    # print('IndexError at %i' % count)
+                    continue
+                # print(d)
+                self[d['query_id']].append(d)
+        assert count == 1967274
         return self
 
 
